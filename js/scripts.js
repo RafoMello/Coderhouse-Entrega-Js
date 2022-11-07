1e3e50be540cea909cef1a2221db690f56ace471
RafoMello <mello.rafaelsantos@gmail.com>
2022-11-06 22:34:13 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 let total_alunos = prompt("Total de Alunos");
 let i=0;
-var nota = new array();
+var nota = [];
 let validacionclase;
 
-for(i=0;i<=total_alunos;i++)
+for(i=0;i<total_alunos;i++)
 {
-nota.push(prompt("Insira nota del aluno " & i));
+nota.push(prompt(`Insira nota del aluno ${i}`));
 }
 
-validacionclase = promedioclase(nota,total_alunos);
+validacionclase = parseInt(promedioclase(nota,total_alunos));
 
 if(validacionclase>=6)
 {
@@ -23,7 +23,7 @@ function promedioclase (nota, total_alunos)
     let promedionota;
     let n;
 
-    for(n=0;n<=total_alunos;n++)
+    for(n=0;n<total_alunos;n++)
     {
         promedionota = promedionota+nota[n];
     }
